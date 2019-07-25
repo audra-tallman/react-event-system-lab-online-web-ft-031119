781728b2fac066bbb86977dcd7e62542c03a74ed
Audra Tallman <audi.tallman@gmail.com>
2019-07-25 02:03:02 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+ class Keypad extends React.Component {
+   
+   handleInputPassword = () => console.log('Entering password...')
+ 
+   render() {
+     return (
+       )
+   }
+   
+ }
